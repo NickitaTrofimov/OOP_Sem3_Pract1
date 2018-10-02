c6b770ba31739d10ca0bfecbe2cf9c308f375459
NickitaTrofimov <32363673+NickitaTrofimov@users.noreply.github.com>
2018-10-02 22:44:47 +0300

    Create BallTester.java

---
+++ BallTester.java
@@ -0,0 +1,8 @@
+public class BallTester {
+    public static	void	main(String[]	args)	{
+
+        Ball ball =	new Ball("большущий",	"красно-синий",	"высоко",2000);
+
+        System.out.println(ball);
+    }
+}
